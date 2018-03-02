ee7fec5baa76aea62e08860f3b89a5e6f04b3bb8
charlespetchsy <petchsycp@gmail.com>
2018-03-01 20:50:06 -0500

    create python function

--- Python/randCount.py
+++ Python/randCount.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python
 
 import random
-SIZE = 100000
-aList = []
-for i in range(SIZE):
-    getNum = random.randint(0,9)
-    aList.append(getNum)
 
-print([[x, (aList.count(x) / SIZE)] for x in set(aList)])
+def pythonRNG(n):
+
+    n = 100000
+    aList = []
+    for i in range(n):
+        getNum = random.randint(0,9)
+        aList.append(getNum)
+
+    return ([[x, (aList.count(x) / n)] for x in set(aList)])
+
+def ogRNG(n):
+    return None
+
+
+if __name__ == "__main__":
+
+    print ("Python's built-in RNG (import random):")
+    for pair in pythonRNG(1000000000):
+        print (pair)
